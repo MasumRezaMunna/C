3adb3aeb15e03070bef1f5a908e9fc4b52c1fee8
MasumRezaMunna <masumrezamunna2005@gmail.com>
2026-04-09 11:34:32 +0600

    all in one

---
+++ allInOne.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+// user defined
+
+struct student {
+    int roll;
+    float cgpa;
+    char name[100];
+};
+
+int main(){
+    struct student s1;
+    s1.roll = 1664;
+    s1.cgpa = 9.2;
+
+    strcpy(s1.name,"Munna");
+
+    printf("student name = %s\n", s1.name);
+    printf("student roll no = %d\n", s1.roll);
+    printf("student name = %f\n", s1.cgpa);
+
+    return 0;
+}
